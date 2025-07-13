9a67bf9a532dc955a159379215896cbf2265cef3
Ryan079 <ryan079077@gmail.com>
2025-07-12 23:28:47 -0300

    Criação do diretório "backend" e configuração do Spring Boot

---
+++ backend/src/main/java/br/edu/ufape/plataformaeventos/PlataformaeventosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.plataformaeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaeventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaeventosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/edu/ufape/plataformaeventos/PlataformaeventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.plataformaeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaeventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
