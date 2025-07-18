461604d562c9cb8fd188429d6855dc15ba65e738
Ryan079 <ryan079077@gmail.com>
2025-07-18 04:01:35 -0300

    Implementação do DTO de User

---
+++ backend/src/main/java/br/edu/ufape/plataformaeventos/dto/UserDTO.java
@@ -0,0 +1,65 @@
+package br.edu.ufape.plataformaeventos.dto;
+
+import br.edu.ufape.plataformaeventos.util.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UserDTO {
+
+    @NotBlank(message = "Nome não pode ser vazio")
+    private String name;
+    
+    @NotBlank(message = "Email não pode ser vazio")
+    @Email(message = "Email inválido")
+    private String email;
+
+    @NotBlank(message = "Senha não pode ser vazia")
+    @Size(min = 8, message = "Senha deve ter pelo menos 8 caracteres")
+    private String password;
+    
+    private UserRole role;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String name, String email, String password, UserRole role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+}
