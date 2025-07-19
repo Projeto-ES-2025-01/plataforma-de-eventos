f1ef5fb666af496e87b8478b656ae5f7b3f15702
Guilherme <guilhermealmeidagama@gmail.com>
2025-07-19 14:42:48 -0300

    criando os components de login e register, adicionando os services e guards necessarios para autenticacao, atualizando as routes para incluir as novas rotas e atualizando as dependencias para incluir o JWT e criando o component home para testar o authGuard

---
+++ frontend/src/app/auth/register/register.ts
@@ -0,0 +1,41 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './register.html',
+  styleUrls: ['./register.css']
+})
+export class RegisterComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+
+  registerForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]]
+  }, { validators: this.passwordMatchValidator });
+
+  passwordMatchValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const { email, password } = this.registerForm.value;
+      const newUser = { id: 0, email, password, role: 'user' };
+
+      this.authService.register(newUser)
+      .then(() => this.router.navigate(['/login']))
+      .catch(err => alert('Error registering user: ' + err));
+    }
+  }
+}
