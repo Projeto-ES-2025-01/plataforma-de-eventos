e7cda06291850858768bde79e8e6aef3764dcef1
Ryan079 <ryan079077@gmail.com>
2025-07-27 16:10:26 -0300

    Criação de OrganizerProfileService

--- backend/src/main/java/br/edu/ufape/plataformaeventos/service/OrganizerProfileService.java
+++ backend/src/main/java/br/edu/ufape/plataformaeventos/service/OrganizerProfileService.java
@@ -1,5 +1,35 @@
 package br.edu.ufape.plataformaeventos.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.ufape.plataformaeventos.dto.UserDTO;
+import br.edu.ufape.plataformaeventos.model.OrganizerProfile;
+import br.edu.ufape.plataformaeventos.model.User;
+import br.edu.ufape.plataformaeventos.repository.OrganizerProfileRepository;
+import br.edu.ufape.plataformaeventos.repository.UserRepository;
+import br.edu.ufape.plataformaeventos.util.UserRole;
+import jakarta.transaction.Transactional;
+
+@Service
 public class OrganizerProfileService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private OrganizerProfileRepository organizerProfileRepository;
+
+    @Transactional
+    public OrganizerProfile createOrganizerProfile(UserDTO userDTO) {
+        User user = new User(userDTO.getName(),
+                userDTO.getEmail(),
+                userDTO.getPassword(),
+                UserRole.ORGANIZER);
+        user = userRepository.save(user);
+
+        OrganizerProfile organizerProfile = new OrganizerProfile();
+        organizerProfile.setUser(user);
+        return organizerProfileRepository.save(organizerProfile);
+    }
 }
