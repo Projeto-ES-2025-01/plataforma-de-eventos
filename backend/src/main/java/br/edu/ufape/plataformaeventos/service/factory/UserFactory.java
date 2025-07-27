92211cb30897d8d1ef5ad022918cce5e5d10116d
Ryan079 <ryan079077@gmail.com>
2025-07-27 03:49:58 -0300

    Remoção de Factory

--- backend/src/main/java/br/edu/ufape/plataformaeventos/service/factory/UserFactory.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ufape.plataformaeventos.service.factory;
-
-import br.edu.ufape.plataformaeventos.dto.UserDTO;
-import br.edu.ufape.plataformaeventos.model.User;
-
-public interface UserFactory {
-    User createUser(UserDTO userDTO);
-}
