f242826b03d17f4f046ef836b296a5dc0ee7f9c7
Ruan-Alves-Paz <156556770+RuanDiego2004@users.noreply.github.com>
2025-07-27 22:11:46 -0300

    Implementação do EventRepository

---
+++ backend/src/main/java/br/edu/ufape/plataformaeventos/repository/EventRepository.java
@@ -0,0 +1,16 @@
+package br.edu.ufape.plataformaeventos.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ufape.plataformaeventos.model.Event;
+
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    List<Event> findByNameIgnoreCase(String name);
+    
+}
