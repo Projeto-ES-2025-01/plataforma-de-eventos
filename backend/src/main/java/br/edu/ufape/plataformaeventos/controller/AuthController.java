d9ef25096542489c4cc0e0f630b96bb640f2d253
Ryan079 <ryan079077@gmail.com>
2025-07-28 04:49:00 -0300

    Refatoração de EndPoint register para que seja possível cadastrar Student ou Organizer

--- backend/src/main/java/br/edu/ufape/plataformaeventos/controller/AuthController.java
+++ backend/src/main/java/br/edu/ufape/plataformaeventos/controller/AuthController.java
@@ -4,14 +4,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ufape.plataformaeventos.dto.AuthDTO;
 import br.edu.ufape.plataformaeventos.dto.LoginResponseDTO;
-import br.edu.ufape.plataformaeventos.dto.UserDTO;
+import br.edu.ufape.plataformaeventos.dto.UserRegistrationDTO;
 import br.edu.ufape.plataformaeventos.model.User;
 import br.edu.ufape.plataformaeventos.security.TokenService;
 import br.edu.ufape.plataformaeventos.service.AuthService;
+import br.edu.ufape.plataformaeventos.util.UserRole;
 import jakarta.validation.Valid;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -40,9 +45,14 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody @Valid UserDTO userDTO) {
-        User registeredUser = this.authService.register(userDTO);
+    public ResponseEntity<?> register(@RequestBody @Valid UserRegistrationDTO userRegistrationDTO) {
+        if(userRegistrationDTO.getStudentProfileDTO() != null) {
+            userRegistrationDTO.getUserDTO().setRole(UserRole.STUDENT);
+        } else {
+            userRegistrationDTO.getUserDTO().setRole(UserRole.ORGANIZER);
+        }
         
-        return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
+        Object result = authService.register(userRegistrationDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 }
