eb0a177ea1a9d3ba52e81550c7fc77fa92efef7f
Renato Beltrão <renatinbeltrao@gmail.com>
2025-08-02 15:45:20 -0300

    Adicionando addParticipant que adiciona o estudante a um evento e o evento a estudante e getEventsOfStudent que retorna os DTO's dos eventos que o Student participa.

--- backend/src/main/java/br/edu/ufape/plataformaeventos/service/StudentProfileService.java
+++ backend/src/main/java/br/edu/ufape/plataformaeventos/service/StudentProfileService.java
@@ -1,15 +1,22 @@
 package br.edu.ufape.plataformaeventos.service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import br.edu.ufape.plataformaeventos.dto.EventDTO;
 import br.edu.ufape.plataformaeventos.dto.StudentProfileDTO;
 import br.edu.ufape.plataformaeventos.dto.UserDTO;
+import br.edu.ufape.plataformaeventos.model.Event;
 import br.edu.ufape.plataformaeventos.model.StudentProfile;
 import br.edu.ufape.plataformaeventos.model.User;
+import br.edu.ufape.plataformaeventos.repository.EventRepository;
 import br.edu.ufape.plataformaeventos.repository.StudentProfileRepository;
 import br.edu.ufape.plataformaeventos.repository.UserRepository;
 import br.edu.ufape.plataformaeventos.util.UserRole;
@@ -24,6 +31,9 @@ public class StudentProfileService {
 
     @Autowired
     private StudentProfileRepository studentProfileRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
 
     @Transactional
     public StudentProfile createStudentProfile(UserDTO userDTO, StudentProfileDTO studentProfileDTO) {
@@ -81,9 +91,39 @@ public class StudentProfileService {
         studentProfileRepository.delete(studentProfile);
     }
 
+    public StudentProfile findByCPF(String cpf){
+        return studentProfileRepository.findByCpf(cpf);
+    }
+
     public List<StudentProfileDTO> getAllStudentProfiles() {
         return studentProfileRepository.findAll().stream()
                 .map(StudentProfile::toDTO)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void addParticipant(Event event,StudentProfile student){
+
+        if (student == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Estudante não encontrado");
+        }
+        event.addParticipant(student);
+        eventRepository.save(event);
+
+        student.addEvent(event);
+        studentProfileRepository.save(student);
+    }
+
+    public Set<EventDTO> getEventsOfStudent(String email) {
+        StudentProfile studentProfile = studentProfileRepository.findByUserEmail(email);
+        if (studentProfile == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Estudante não encontrado");
+        }
+        Set<Event> eventos = studentProfile.getEvents();
+        Set<EventDTO> eventosDTO = new HashSet<>(); 
+        for(Event eventoTemp: eventos){
+            eventosDTO.add(eventoTemp.eventToEventDTO());
+        }
+        return eventosDTO;
     }
 }
