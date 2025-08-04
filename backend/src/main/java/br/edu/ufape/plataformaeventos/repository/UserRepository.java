0bb838f556862e8fa36891c318fc09f5f0ab7ea1
Ruan-Alves-Paz <156556770+RuanDiego2004@users.noreply.github.com>
2025-08-04 15:06:31 -0300

    Implementação do método deleteByEmail

--- backend/src/main/java/br/edu/ufape/plataformaeventos/repository/UserRepository.java
+++ backend/src/main/java/br/edu/ufape/plataformaeventos/repository/UserRepository.java
@@ -5,10 +5,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import br.edu.ufape.plataformaeventos.model.User;
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     
     UserDetails findByEmail(String email);
 
+    @Transactional
+    void deleteByEmail(String email);
 }
