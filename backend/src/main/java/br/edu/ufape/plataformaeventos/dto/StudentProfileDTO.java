6c77c135857f03e259004857f02a2ea0c14fdc5c
Renato Beltrão <renatinbeltrao@gmail.com>
2025-08-07 01:11:44 -0300

    Adicionando novas validações a StudentProfile

--- backend/src/main/java/br/edu/ufape/plataformaeventos/dto/StudentProfileDTO.java
+++ backend/src/main/java/br/edu/ufape/plataformaeventos/dto/StudentProfileDTO.java
@@ -3,22 +3,36 @@ package br.edu.ufape.plataformaeventos.dto;
 import java.time.LocalDate;
 
 import br.edu.ufape.plataformaeventos.util.DegreeProgram;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class StudentProfileDTO {
 
     @NotBlank(message = "Nome completo é obrigatório")
     private String fullName;
+
     @NotBlank(message = "CPF é obrigatório")
+    @Pattern(regexp = "^(?=.*[0-9])[0-9\\p{Punct}]+$", message = "CPF deve conter apenas números e símbolos, com pelo menos um número")
     private String cpf;
+
     @NotNull(message = "Data de nascimento é obrigatória")
+    @Past(message = "Data de nascimento deve ser no passado")
     private LocalDate birthDate;
+
     @NotBlank(message = "Número de telefone é obrigatório")
+    @Pattern(regexp = "^(?=.*[0-9])[0-9\\p{Punct}]+$", message = "Numero deve conter apenas números e símbolos, com pelo menos um número")
+    @Size(min =10,message = "Numero deve conter ao menos 11 caracteres")
     private String phoneNumber;
+
     @NotNull(message = "Curso é obrigatório")
     private DegreeProgram degreeProgram;
+
     @NotNull(message = "Período atual é obrigatório")
+    @Min(value = 1, message = "Período atual deve ser maior que 0")
     private int currentPeriod;
 
     public String getFullName() {
