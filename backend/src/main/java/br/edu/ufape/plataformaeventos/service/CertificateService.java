6b0fbea095c25e38196fc2157c06709bd02af6e8
Ryan079 <ryan079077@gmail.com>
2025-08-31 01:10:55 -0300

    Criação de CertificateService

---
+++ backend/src/main/java/br/edu/ufape/plataformaeventos/service/CertificateService.java
@@ -0,0 +1,31 @@
+package br.edu.ufape.plataformaeventos.service;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import br.edu.ufape.plataformaeventos.model.Certificate;
+import br.edu.ufape.plataformaeventos.model.Event;
+import br.edu.ufape.plataformaeventos.model.StudentProfile;
+import br.edu.ufape.plataformaeventos.repository.CertificateRepository;
+
+@Service
+public class CertificateService {
+
+    private CertificateRepository certificateRepository;
+
+    public CertificateService(CertificateRepository certificateRepository) {
+        this.certificateRepository = certificateRepository;
+    }
+
+    public Certificate EmitirCertificado(Event event, StudentProfile participant) {
+        String code = gerarCodigoUnico();
+        Certificate certificate = new Certificate(event, participant, LocalDate.now(), code);
+        return certificateRepository.save(certificate);
+    }
+
+    private String gerarCodigoUnico() {
+        return UUID.randomUUID().toString();
+    }
+}
