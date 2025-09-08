9a8c6964f90627bbd43ac74e49d546b60d07fd80
Ryan079 <ryan079077@gmail.com>
2025-09-07 23:11:21 -0300

    adicionando anotação de repositório no certificateRepository

--- backend/src/main/java/br/edu/ufape/plataformaeventos/repository/CertificateRepository.java
+++ backend/src/main/java/br/edu/ufape/plataformaeventos/repository/CertificateRepository.java
@@ -3,11 +3,13 @@ package br.edu.ufape.plataformaeventos.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.edu.ufape.plataformaeventos.model.Certificate;
 import br.edu.ufape.plataformaeventos.model.Event;
 import br.edu.ufape.plataformaeventos.model.StudentProfile;
 
+@Repository
 public interface CertificateRepository extends JpaRepository<Certificate, Long> {
     List<Certificate> findByParticipant(StudentProfile participant);
     List<Certificate> findByEventId(Event event);
